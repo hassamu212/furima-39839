abbfa47008567e5dba63a601def85ad6166303de
hassamu212 <shigeakidaidouji@gmail.com>
2023-12-26 09:55:30 +0900

    ActiveHashとアソシエーションとバリデーション㏈のマイグレーション完了

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,17 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :delivery_charge
+  belongs_to :prefecture
+  belongs_to :delivery_day
+
+  #空の投稿を保存できないようにする
+  validates :title, :text, presence: true
+  #ジャンルの選択が「---」の時は保存できないようにする
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank"}
+  validates :status_id, numericality: { other_than: 1, message: "can't be blank"}
+  validates :delivery_charge_id, numericality: { other_than: 1, message: "can't be blank"}
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank"}
+  validates :delivery_day_id, numericality: { other_than: 1, message: "can't be blank"}
 end
