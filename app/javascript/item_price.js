68e8832e7eaa610ff1f509e1e73edbf239654c0d
hassamu212 <shigeakidaidouji@gmail.com>
2023-12-31 16:05:47 +0900

    Javascript

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,15 +1,15 @@
-document.addEventListener("DOMContentLoaded", function() {
-  
-  const priceInput = document.getElementById("item-price");
-  const addTaxDom = document.getElementById("add-tax-price");
-  const profit = document.getElementById("profit");
-  
-  priceInput.addEventListener("input", () => {
-    const inputValue = parseInt(priceInput.value);
-    const addTax = Math.floor(inputValue * 0.1);
+function handleTurboLoad() {
+  if (document.getElementById('item-price') != null) {
+    const priceInput = document.getElementById("item-price");
+    const addTaxDom = document.getElementById("add-tax-price");
+    const itemProfit = document.getElementById("profit");
 
-    addTaxDom.innerHTML = addTax;
-    profit.innerHTML = Math.floor(inputValue - addTax);
-  });
+    priceInput.addEventListener("input", () => {
+      const inputValue = priceInput.value;
+      addTaxDom.innerHTML = Math.floor(inputValue * 0.1);
+      itemProfit.innerHTML = Math.ceil(inputValue - (inputValue * 0.1));
+    });
+  };
+};
 
-});
+window.addEventListener('turbo:load', handleTurboLoad);
