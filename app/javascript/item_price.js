482c97cc3ec36c7de6413e96a43bdec4744caa6f
hassamu212 <shigeakidaidouji@gmail.com>
2024-01-03 08:04:14 +0900

    細かい修正

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,15 +1,14 @@
-function handleTurboLoad() {
-  if (document.getElementById('item-price') != null) {
+window.addEventListener('turbo:load', () => {
+  if ( document.getElementById('item-price')!= null) {
     const priceInput = document.getElementById("item-price");
     const addTaxDom = document.getElementById("add-tax-price");
-    const itemProfit = document.getElementById("profit");
+    const profitDom = document.getElementById("profit");
 
     priceInput.addEventListener("input", () => {
-      const inputValue = priceInput.value;
-      addTaxDom.innerHTML = Math.floor(inputValue * 0.1);
-      itemProfit.innerHTML = Math.ceil(inputValue - (inputValue * 0.1));
-    });
-  };
-};
-
-window.addEventListener('turbo:load', handleTurboLoad);
+      const inputValue = document.getElementById("item-price").value;
+        let tax = Math.floor(inputValue * 0.1)
+        addTaxDom.innerHTML = tax ;
+        profitDom.innerHTML = Math.ceil(inputValue * 0.9)
+    })
+  }
+});
