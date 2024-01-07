2a554cb0b33b5b50e60309bf5f8de6ded6fd1844
hassamu212 <shigeakidaidouji@gmail.com>
2024-01-07 16:44:37 +0900

    編集機能、ログイン有無、エラーメッセージの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: :new
+  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :move_to_index, except: [:index, :new, :show]
   
   def index
      @items = Item.all.order("created_at DESC")
@@ -26,9 +27,24 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
   
+  def update
+    if @item.update(item_params)
+      redirect_to item_path(@item.id)
+    else
+      render 'items/edit', status: :unprocessable_entity
+    end
+  end
+
   private
   def item_params
     params.require(:item).permit(:item_title, :item_concept, :price, :category_id, :status_id, :delivery_charge_id, :prefecture_id, :delivery_day_id, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    @item = Item.find(params[:id])
+    unless @item && current_user.id == @item.user_id
+      redirect_to action: :index
+    end
   end
   
 end
