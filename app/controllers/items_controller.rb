20dcfa2b39556ec3f6eebecc89b652e2e876dd3b
hassamu212 <shigeakidaidouji@gmail.com>
2024-01-08 14:20:00 +0900

    before_action :move_to_indexの順番入れ替え、アクションの整理

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit]
-  before_action :move_to_index, except: [:index, :new, :show]
   before_action :set_item, only: [:show, :edit]
+  before_action :move_to_index, except: [:index, :new, :show]
   
   def index
      @items = Item.all.order("created_at DESC")
@@ -40,7 +40,6 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    @item = Item.find(params[:id])
     unless @item && current_user.id == @item.user_id
       redirect_to action: :index
     end
