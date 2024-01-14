9f7894ed12e150aaa7c1f202e576947cc4f89cf6
hassamu212 <shigeakidaidouji@gmail.com>
2024-01-14 13:41:25 +0900

    orderコントローラー修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,4 +4,21 @@ class OrdersController < ApplicationController
   def index
     @items = Item.all
   end
+
+  def create
+    @Order = Order.create(order_params)
+    Dliveryaddresses.create(deliveryaddresses_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def order_params
+    params.merge(user_id: current_user.id, item_id: @item_id)
+  end
+
+  def deliveryaddresses_params
+    params.permit(:address, :prefecture_id, :municipalities, :street_address, :building_name, :telephone_number).merge(order_id: @order.id)
+  end
+
 end
