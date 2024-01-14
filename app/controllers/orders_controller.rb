aa6ffa969edd974a4da6a0693db856c49fb22368
hassamu212 <shigeakidaidouji@gmail.com>
2024-01-14 14:47:52 +0900

    Formオブジェクトパターン実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,19 +6,20 @@ class OrdersController < ApplicationController
   end
 
   def create
-    @Order = Order.create(order_params)
-    Dliveryaddresses.create(deliveryaddresses_params)
-    redirect_to root_path
+    @Orderaddress = Orderaddress.new
+    @Orderaddress = Orderaddress.new(order_params)
+    if @orderaddress.valid?
+      @orderaddress.save
+      redirect_to root_path
+    else
+      render :index, status: :unprocessable_entity
+    end
   end
 
   private
 
   def order_params
-    params.merge(user_id: current_user.id, item_id: @item_id)
-  end
-
-  def deliveryaddresses_params
-    params.permit(:address, :prefecture_id, :municipalities, :street_address, :building_name, :telephone_number).merge(order_id: @order.id)
+    params.require(:orderaddress).permit(:address, :prefecture_id, :municipalities, :street_address, :building_name, :telephone_number, :use_id, :item_id).merge(user_id: current_user.id)
   end
 
 end
